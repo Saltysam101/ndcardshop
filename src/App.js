be60d635c9ee2243cfb0a812b30b5036a9523502
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2021-05-18 13:23:23 -0400

    set up router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Shop from './pages/shop';
 
 function App() {
   return (
+  <Router>  
     <div className="">
-      <Shop />
+      <Switch>
+        <Route path="/" exact component={Shop}/>
+      </Switch>
     </div>
+  </Router>  
   );
 }
 
