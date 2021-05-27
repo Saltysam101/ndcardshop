c0698f8c5e9ca261795181670eae41b26a575a45
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2021-05-27 09:08:07 -0400

    implemented general look and feel of shop for mobile

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -2,8 +2,40 @@ import React from 'react'
 
 export default function Shop() {
     return (
-        <div className="relative h-screen mt-16 bg-offblack text-white">
-           <h3 className="text-white">Hello There!</h3> 
+        <div className="h-auto mb-16 pb-6 mt-16 bg-offblack text-white">
+            <div>
+                <div className="w-96 pt-4 mb-4 mx-auto">
+                    <div className="h-60 mt-4 bg-gray-600 text-center">Image</div>
+                    <h2 className="text-lg font-bold mt-4">Item Name</h2>
+                    <p className="text-base mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                    <div className="flex mt-4 justify-between">
+                        <h3 className="text-xl font-bold">$49.99</h3>
+                        <button className="bg-blue-300 w-24 h-10">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="w-96 pt-4 mb-4 mx-auto">
+                    <div className="h-60 mt-4 bg-gray-600 text-center">Image</div>
+                    <h2 className="text-lg font-bold mt-4">Item Name</h2>
+                    <p className="text-base mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                    <div className="flex mt-4 justify-between">
+                        <h3 className="text-xl font-bold">$49.99</h3>
+                        <button className="bg-blue-300 w-24 h-10">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="w-96 pt-4 mx-auto">
+                    <div className="h-60 mt-4 bg-gray-600 text-center">Image</div>
+                    <h2 className="text-lg font-bold mt-4">Item Name</h2>
+                    <p className="text-base mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                    <div className="flex mt-4 justify-between">
+                        <h3 className="text-xl font-bold">$49.99</h3>
+                        <button className="bg-blue-300 w-24 h-10">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
