fe80dca3548c8109b2889874342afd8ba09054d5
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2021-06-02 08:11:50 -0400

    started styling shop page for larger screen sizes and added custom font breakpoints to html el

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -2,37 +2,100 @@ import React from 'react'
 
 export default function Shop() {
     return (
-        <div className="h-auto mb-16 pb-6 mt-16 bg-offblack text-white">
+        <div className="h-auto mb-16 pb-6 mt-16 bg-offblack text-white md:mb-0">
             <div>
-                <div className="w-96 pt-4 mb-4 mx-auto">
-                    <div className="h-60 mt-4 bg-gray-600 text-center">Image</div>
-                    <h2 className="text-lg font-bold mt-4">Item Name</h2>
-                    <p className="text-base mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
-                    <div className="flex mt-4 justify-between">
-                        <h3 className="text-xl font-bold">$49.99</h3>
-                        <button className="bg-blue-300 w-24 h-10">Add to Cart</button>
+                <h2 className="text-lg ml-4 pt-4">A group of cards</h2>
+                <div className="lg:flex lg:flex-wrap lg:justify-evenly">
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
                     </div>
                 </div>
             </div>
             <div>
-                <div className="w-96 pt-4 mb-4 mx-auto">
-                    <div className="h-60 mt-4 bg-gray-600 text-center">Image</div>
-                    <h2 className="text-lg font-bold mt-4">Item Name</h2>
-                    <p className="text-base mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
-                    <div className="flex mt-4 justify-between">
-                        <h3 className="text-xl font-bold">$49.99</h3>
-                        <button className="bg-blue-300 w-24 h-10">Add to Cart</button>
+            <h2 className="text-lg ml-4 pt-4">A group of cards</h2>
+            <div className="lg:flex lg:flex-wrap lg:justify-evenly">
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
                     </div>
                 </div>
             </div>
             <div>
-                <div className="w-96 pt-4 mx-auto">
-                    <div className="h-60 mt-4 bg-gray-600 text-center">Image</div>
-                    <h2 className="text-lg font-bold mt-4">Item Name</h2>
-                    <p className="text-base mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
-                    <div className="flex mt-4 justify-between">
-                        <h3 className="text-xl font-bold">$49.99</h3>
-                        <button className="bg-blue-300 w-24 h-10">Add to Cart</button>
+            <h2 className="text-lg ml-4 pt-4">A group of cards</h2>
+            <div className="lg:flex lg:flex-wrap lg:justify-evenly">
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
                     </div>
                 </div>
             </div>
