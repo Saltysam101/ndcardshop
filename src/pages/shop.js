3e24fa259738ea3512bf74189995a767179b1adf
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2021-06-03 08:24:14 -0400

    worked on lg screen layout for shop

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -4,10 +4,10 @@ export default function Shop() {
     return (
         <div className="h-auto mb-16 pb-6 mt-16 bg-offblack text-white md:mb-0">
             <div>
-                <h2 className="text-lg ml-4 pt-4">A group of cards</h2>
-                <div className="lg:flex lg:flex-wrap lg:justify-evenly">
+                <h2 className="text-lg ml-4 pt-4 lg:pt-10">A group of cards</h2>
+                <div className="lg:flex lg:flex-wrap lg:justify-start lg:mt-4 xl:justify-between">
                     <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
                         <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
                         <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
                         <div className="flex mt-4 justify-between">
@@ -16,7 +16,7 @@ export default function Shop() {
                         </div>
                     </div>
                     <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
                         <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
                         <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
                         <div className="flex mt-4 justify-between">
@@ -25,39 +25,7 @@ export default function Shop() {
                         </div>
                     </div>
                     <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
-                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
-                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
-                        <div className="flex mt-4 justify-between">
-                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
-                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-            <h2 className="text-lg ml-4 pt-4">A group of cards</h2>
-            <div className="lg:flex lg:flex-wrap lg:justify-evenly">
-                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
-                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
-                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
-                        <div className="flex mt-4 justify-between">
-                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
-                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
-                        </div>
-                    </div>
-                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
-                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
-                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
-                        <div className="flex mt-4 justify-between">
-                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
-                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
-                        </div>
-                    </div>
-                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
                         <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
                         <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
                         <div className="flex mt-4 justify-between">
@@ -68,10 +36,10 @@ export default function Shop() {
                 </div>
             </div>
             <div>
-            <h2 className="text-lg ml-4 pt-4">A group of cards</h2>
-            <div className="lg:flex lg:flex-wrap lg:justify-evenly">
+            <h2 className="text-lg ml-4 pt-4 lg:pt-10">A group of cards</h2>
+            <div className="lg:flex lg:flex-wrap lg:justify-start lg:mt-4 xl:justify-between">
                     <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
                         <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
                         <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
                         <div className="flex mt-4 justify-between">
@@ -80,7 +48,7 @@ export default function Shop() {
                         </div>
                     </div>
                     <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
                         <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
                         <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
                         <div className="flex mt-4 justify-between">
@@ -89,7 +57,39 @@ export default function Shop() {
                         </div>
                     </div>
                     <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
-                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-48">Image</div>
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+            <h2 className="text-lg ml-4 pt-4 lg:pt-10">A group of cards</h2>
+            <div className="lg:flex lg:flex-wrap lg:justify-start lg:mt-4 xl:justify-between">
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
+                        <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
+                        <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+                        <div className="flex mt-4 justify-between">
+                            <h3 className="text-xl font-bold lg:text-base">$49.99</h3>
+                            <button className="bg-blue-600 text-lg w-32 h-10">ADD TO CART</button>
+                        </div>
+                    </div>
+                    <div className="w-96 pt-4 mb-4 mx-auto md:text-lg lg:w-64 lg:mx-10">
+                        <div className="h-60 mt-4 bg-gray-600 text-center lg:h-36">Image</div>
                         <h2 className="text-lg font-bold mt-4 lg:text-base">Item Name</h2>
                         <p className="text-base mt-4 lg:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
                         <div className="flex mt-4 justify-between">
